e732a241aa7a08609faf0f78bd79b3b9def726bc
Yvette Cook <yvettecook@gmail.com>
2017-07-14 12:12:14 +0100

    Correct minor identifier

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "JWTProvider",
     dependencies: [
         // JSON Web Tokens in Swift by @siemensikkema.
-        .Package(url:"https://github.com/yvettecook/jwt.git", majorVersion: 2, minorVersion: 2),
+        .Package(url:"https://github.com/yvettecook/jwt.git", majorVersion: 2, minor: 2),
 
     	// Middleware and conveniences for using Auth in Vapor.
         .Package(url:"https://github.com/vapor/auth-provider.git", majorVersion: 1),
